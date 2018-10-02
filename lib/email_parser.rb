8b586e6ae8afb21c6ed371ec94b9e8298a211505
Samuel Cornet <samuel@noukod.com>
2018-10-02 20:01:07 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailParser
   end
 
   def parse
-    # email_array = self.new
+     email_array = self.new
     # email_array = @emails.split(",")
   email_array.collect { |e| e.split(",")}
     email_array.uniq
